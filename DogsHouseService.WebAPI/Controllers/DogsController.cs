674917f9a54fb490c9c8c4c9c050838a64a97464
ayomanetsan <vladyslav.voropaiev1@nure.ua>
2023-05-30 16:28:50 +0300

    Create DogsController

---
+++ DogsHouseService.WebAPI/Controllers/DogsController.cs
@@ -0,0 +1,21 @@
+﻿using DogsHouseService.BLL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DogsHouseService.WebAPI.Controllers
+{
+    public class DogsController : Controller
+    {
+        private readonly IDogService _dogService;
+
+        public DogsController(IDogService dogService)
+        {
+            _dogService = dogService;
+        }
+
+        [HttpGet("ping")]
+        public ActionResult GetApiVersion()
+        {
+            return Ok("Dogs house service. Version 1.0.1");
+        }
+    }
+}
