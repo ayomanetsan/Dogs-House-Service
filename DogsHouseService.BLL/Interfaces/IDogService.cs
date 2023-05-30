17368cc8ee63b82be51411e43b5322ecc9346e99
ayomanetsan <vladyslav.voropaiev1@nure.ua>
2023-05-30 15:45:15 +0300

    Create IDogService interface

---
+++ DogsHouseService.BLL/Interfaces/IDogService.cs
@@ -0,0 +1,16 @@
+﻿using DogsHouseService.DAL.Entities;
+using Microsoft.Data.SqlClient;
+
+namespace DogsHouseService.BLL.Interfaces
+{
+    public interface IDogService
+    {
+        Task<IEnumerable<Dog>> GetAllDogsAsync();
+
+        Task<IEnumerable<Dog>> GetSortedDogsAsync(string sortBy, SortOrder sortOrder);
+
+        Task<IEnumerable<Dog>> GetPagedDogsAsync(int pageNumber, int pageSize);
+
+        Task<IEnumerable<Dog>> GetPagedAndSortedDogsAsync(int pageNumber, int pageSize, string sortBy, SortOrder sortOrder);
+    }
+}
