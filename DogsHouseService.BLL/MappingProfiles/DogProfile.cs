d022befa9c1e0094bcd27de7aa5a618732b42117
ayomanetsan <vladyslav.voropaiev1@nure.ua>
2023-05-31 13:17:21 +0300

    Create dog mapping profile

---
+++ DogsHouseService.BLL/MappingProfiles/DogProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using DogsHouseService.Common.DTO.Dog;
+using DogsHouseService.DAL.Entities;
+
+namespace DogsHouseService.BLL.MappingProfiles
+{
+    internal class DogProfile : Profile
+    {
+        public DogProfile()
+        {
+            CreateMap<Dog, DogDto>();
+        }
+    }
+}
